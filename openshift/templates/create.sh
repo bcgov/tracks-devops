46d3cb86f63e6c6031dad7380823595581506495
Robert Johnstone <rob@plasticviking.com>
2020-12-10 23:09:56 +0800

    updated configs for node backend

--- openshift/templates/create.sh
+++ openshift/templates/create.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [[ -z "${ENV_NAME}" || -z "${API_BASE}" ]]
+if [[ -z "${ENV_NAME}" || -z "${API_BASE}" || -z "${KEYCLOAK_REALM}" || -z "${KEYCLOAK_URL}" || -z "${JWT_ISSUER_URI}" ]]
 then
       echo "Please set required environment vars before running"
       exit 2
@@ -11,15 +11,14 @@ DATABASE_REPLICATION_PASSWORD=`openssl rand -base64 40 |  tr -cd '[:alpha:]' |  
 DATABASE_ROOT_PASSWORD=`openssl rand -base64 40 |  tr -cd '[:alpha:]' |  head -c 20`
 
 # install these one at a time. this is the correct order.
-
 #oc create configmap envoy-config --from-file=envoy-config.yaml
 #oc process -f ./envoy.yaml CPU_REQUEST=200m CPU_LIMIT=500m MEMORY_REQUEST=300M MEMORY_LIMIT=1G ENV_NAME=${ENV_NAME} | oc apply -f -
 #oc process -f ./database-secrets.yaml ENV_NAME=${ENV_NAME} DATABASE_PASSWORD=${DATABASE_PASSWORD} DATABASE_REPLICATION_PASSWORD=${DATABASE_REPLICATION_PASSWORD} DATABASE_ROOT_PASSWORD=${DATABASE_ROOT_PASSWORD} | oc apply -f -
-#oc process -f ./configmap.yaml BACKEND_SPRING_PROFILES=${BACKEND_SPRING_PROFILES} ENV_NAME=${ENV_NAME} | oc apply -f -
+#oc process -f ./configmap.yaml  KEYCLOAK_REALM=${KEYCLOAK_REALM} KEYCLOAK_URL=${KEYCLOAK_URL} JWT_ISSUER_URI=${JWT_ISSUER_URI} ENV_NAME=${ENV_NAME} | oc apply -f -
 
 #oc process -f ./postgres-crunchy.yaml CPU_REQUEST=200m CPU_LIMIT=1000m MEMORY_REQUEST=1000M MEMORY_LIMIT=2G ENV_NAME=${ENV_NAME} | oc apply -f -
 
 #oc process -f ./backend-bc.yaml ENV_NAME=${ENV_NAME} | oc apply -f -
-#oc process -f ./frontend-bc.yaml ENV_NAME=${ENV_NAME} API_BASE=${API_BASE} | oc apply -f -
+oc process -f ./frontend-bc.yaml ENV_NAME=${ENV_NAME} API_BASE=${API_BASE} | oc apply -f -
 #oc process -f ./backend.yaml CPU_REQUEST=400m CPU_LIMIT=1000m MEMORY_REQUEST=1000M MEMORY_LIMIT=2G ENV_NAME=${ENV_NAME} | oc apply -f -
-oc process -f ./frontend.yaml CPU_REQUEST=400m CPU_LIMIT=1000m MEMORY_REQUEST=500M MEMORY_LIMIT=2G ENV_NAME=${ENV_NAME} | oc apply -f -
+#oc process -f ./frontend.yaml CPU_REQUEST=400m CPU_LIMIT=1000m MEMORY_REQUEST=500M MEMORY_LIMIT=2G ENV_NAME=${ENV_NAME} | oc apply -f -
